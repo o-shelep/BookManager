4095c64abe29cfea5846cc9140b5432be0d02e71
shelep-olya <shelep.olya@gmail.com>
2026-05-03 11:21:20 +0300

    chore: initial project setup

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port http://localhost:${PORT}`);
+})
