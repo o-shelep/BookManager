878b55da116ade511b3c77848ad145dec3723445
shelep-olya <shelep.olya@gmail.com>
2026-05-03 11:56:50 +0300

    feat: add uploads set up

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 const routes = require('./src/routes')
 const app = express();
 
+app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', routes);
 
 module.exports = app;
